2517059f2010a878d07ce0827d5e7233606b4f82
Travis Erard <travis@impossiblecreature.com>
2012-03-01 08:45:26 -0600

    added rails app code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AwesomeAdmin::Application.config.secret_token = 'f93bdf0506bf8be54d00f125e6092a5b47b7e1199948d633c49d48cac17757765c8e1b222fa29932f10fc76623fa1f75fc776a4fe9baad9c14ef70a0024637e6'
